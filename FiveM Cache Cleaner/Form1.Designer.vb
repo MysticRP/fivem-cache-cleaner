90201675020e56690fc45c4b4c6d8b2b811d7046
MysticRP <oskarr95z@gmail.com>
2017-12-20 00:28:15 +0100

    fivem cache cleaner
    FiveM Cache Cleaner

---
+++ FiveM Cache Cleaner/Form1.Designer.vb
@@ -0,0 +1,108 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(13, 13)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(146, 21)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Clean Cache "
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(165, 13)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(146, 21)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Start FiveM"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(13, 41)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(298, 26)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Clean Cache and Start FiveM"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.LinkColor = System.Drawing.Color.Red
+        Me.LinkLabel1.Location = New System.Drawing.Point(260, 70)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(51, 13)
+        Me.LinkLabel1.TabIndex = 4
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "oskarr.eu"
+        Me.LinkLabel1.VisitedLinkColor = System.Drawing.Color.DimGray
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Label1.Location = New System.Drawing.Point(236, 72)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(22, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "0.1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.LightGray
+        Me.ClientSize = New System.Drawing.Size(315, 89)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "FiveM Cache Cleaner"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents Label1 As Label
+End Class
---
+++ FiveM Cache Cleaner/Form1.vb
@@ -0,0 +1,40 @@
+﻿Imports System.IO
+Imports System.Environment
+
+Public Class Form1
+    Dim appData As String = GetFolderPath(SpecialFolder.LocalApplicationData)
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Directory.Exists(appData + "/FiveM/FiveM.app/cache/browser/") Or Directory.Exists(appData + "/FiveM/FiveM.app/cache/db/") Or Directory.Exists(appData + "/FiveM/FiveM.app/cache/dunno/") Or Directory.Exists(appData + "/FiveM/FiveM.app/cache/servers/") Or Directory.Exists(appData + "/FiveM/FiveM.app/cache/priv/") Then
+            Directory.Delete(appData + "/FiveM/FiveM.app/cache/browser/", True)
+            Directory.Delete(appData + "/FiveM/FiveM.app/cache/db/", True)
+            Directory.Delete(appData + "/FiveM/FiveM.app/cache/dunno/", True)
+            Directory.Delete(appData + "/FiveM/FiveM.app/cache/servers/", True)
+            Directory.Delete(appData + "/FiveM/FiveM.app/cache/priv/", True)
+            MsgBox("Cache Cleaned !", MsgBoxStyle.Information)
+        Else
+            MsgBox("No Cache Founded !", MsgBoxStyle.Information)
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Process.Start(appData + "/FiveM/FiveM.exe")
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If Directory.Exists(appData + "/FiveM/FiveM.app/cache/browser/") Or Directory.Exists(appData + "/FiveM/FiveM.app/cache/db/") Or Directory.Exists(appData + "/FiveM/FiveM.app/cache/dunno/") Or Directory.Exists(appData + "/FiveM/FiveM.app/cache/servers/") Or Directory.Exists(appData + "/FiveM/FiveM.app/cache/priv/") Then
+            Directory.Delete(appData + "/FiveM/FiveM.app/cache/browser/", True)
+            Directory.Delete(appData + "/FiveM/FiveM.app/cache/db/", True)
+            Directory.Delete(appData + "/FiveM/FiveM.app/cache/dunno/", True)
+            Directory.Delete(appData + "/FiveM/FiveM.app/cache/servers/", True)
+            Directory.Delete(appData + "/FiveM/FiveM.app/cache/priv/", True)
+            Process.Start(appData + "/FiveM/FiveM.exe")
+            MsgBox("Cache Cleaned & FiveM Started !", MsgBoxStyle.Information)
+        Else
+            MsgBox("No Cache Founded !", MsgBoxStyle.Information)
+        End If
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Process.Start("http://oskarr.eu/")
+    End Sub
+End Class
